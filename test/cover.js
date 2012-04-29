f26768fe093798ee6ddfc7d79f2273dcda61d514
Arian <arian@aryweb.nl>
2012-04-29 12:49:26 +0200

    Use the correct property to increment the count of statement calls.

--- test/cover.js
+++ test/cover.js
@@ -3,6 +3,7 @@
 var HTMLReporter = require('../lib/reporters/HTMLReporter');
 
 require('../test-cov/test/fixture.js');
+require('../test-cov/test/fixture2.js');
 
 var reporter = new HTMLReporter(global.__$coverObject);
 console.log(reporter.report());
