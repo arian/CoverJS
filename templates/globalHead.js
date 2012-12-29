1289399b4e58fe8f569e05d9d0e1c3db3144a38f
Arian Stolwijk <stolwijk.arian@gmail.com>
2012-12-29 15:25:40 +0100

    Don't overwrite the results for a module, if they already exist, in the coverInit function

--- templates/globalHead.js
+++ templates/globalHead.js
@@ -4,12 +4,10 @@ if (typeof __$coverObject === "undefined"){
 	else throw new Error("cannot find the global scope");
 }
 var __$coverInit = function(name, code){
-	__$coverObject[name] = {__code: code};
+	if (!__$coverObject[name]) __$coverObject[name] = {__code: code};
 };
 var __$coverInitRange = function(name, range){
-	if (!__$coverObject[name][range]){
-		__$coverObject[name][range] = 0;
-	}
+	if (!__$coverObject[name][range]) __$coverObject[name][range] = 0;
 };
 var __$coverCall = function(name, range){
 	__$coverObject[name][range]++;
--- templates/nodeHead.js
+++ templates/nodeHead.js
@@ -9,12 +9,10 @@ var __$coverCall, __$coverInit, __$coverInitRange;
 		};
 	};
 	__$coverInit = update(function(json, name, code){
-		json[name] = {__code: code};
+		if (!json[name]) json[name] = {__code: code};
 	});
 	__$coverInitRange = update(function(json, name, range){
-		if (!json[name][range]){
-			json[name][range] = 0;
-		}
+		if (!json[name][range]) json[name][range] = 0;
 	});
 	__$coverCall = update(function(json, name, range){
 		json[name][range]++;
